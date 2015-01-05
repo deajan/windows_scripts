0af7fb0cbfce2fd97f4d6280e8bc0b69e1596e7c
Orsiris de Jong <ozy@netpower.fr>
2015-01-05 13:28:08 +0100

    Fixed wrong encoding

--- bin/w32timeConfig.cmd
+++ bin/w32timeConfig.cmd
@@ -2,7 +2,7 @@
 
 :: W32Time config script for Win2K8+
 
-w32tm /config /manualpeerlist:"0.pool.ntp.org 1.pool.ntp.org 2.pool.ntp.org pool.ntp.org",0×8 /syncfromflags:MANUAL /reliable:yes
+w32tm /config /manualpeerlist:"0.pool.ntp.org 1.pool.ntp.org 2.pool.ntp.org pool.ntp.org",0*8 /syncfromflags:MANUAL /reliable:yes
 w32tm /config /update
 net stop w32time
 net start w32time
